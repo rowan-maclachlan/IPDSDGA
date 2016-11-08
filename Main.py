046bfd57ef58e8a6b42cbae762cd1f662e784f37
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2016-11-07 19:09:18 -0600

    I try to understand GIT...

--- Main.py
+++ Main.py
@@ -9,15 +9,46 @@ SIMULATION_STEPS = 100
 
 if __name__ == "__main__":
 
-    cellA = Cell.Cell(0, ps.Position(1,2))
-    cellB = Cell.Cell(1, ps.Position(2,2))
+    # Print and recombinate tests
+
+    cellA = Cell.Cell(0, ps.Position(1, 1))
+    cellB = Cell.Cell(1, ps.Position(1, 2))
+    cellC = Cell.Cell(2, ps.Position(2, 1))
+    cellD = Cell.Cell(3, ps.Position(2, 2))
 
     print str(cellA)
     print str(cellB)
-
-    cellC = Cell.Cell(2, ps.Position(2,3), cellA, cellB)
-
     print str(cellC)
+    print str(cellD)
+
+    cellE = Cell.Cell(2, ps.Position(0, 0), cellA, cellB)
+    cellF = Cell.Cell(2, ps.Position(0, 1), cellC, cellD)
+
+    print str(cellE)
+    print str(cellF)
+
+    # Interaction Tests
+
+    print "\n"
+
+    neighbours = [cellB, cellC, cellD]
+    cellA.interact(neighbours)
+    if not cellA.hasInteracted(cellB): print "err: cellA, cellB"
+    if not cellA.hasInteracted(cellC): print "err: cellA, cellC"
+    if not cellA.hasInteracted(cellD): print "err: cellA, cellD"
+    if cellA.hasInteracted(cellE): print "err: cellA, cellE"
+    if cellA.hasInteracted(cellF): print "err: cellA, cellF"
+
+    neighbours = [cellD, cellE, cellF]
+    cellC.interact(neighbours)
+    if not cellC.hasInteracted(cellD): print "err: cellC, cellD"
+    if not cellC.hasInteracted(cellE): print "err: cellC, cellE"
+    if not cellC.hasInteracted(cellF): print "err: cellC, cellF"
+    if not cellC.hasInteracted(cellA): print "err: cellC, cellA"
+    if cellC.hasInteracted(cellB): print "err: cellC, cellB"
+
+
+
 
 
 
