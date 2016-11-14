3a259fb3f44221baa16638870313c97026d4aa4a
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2016-11-13 19:19:50 -0600

    modularized some tests

--- Main.py
+++ Main.py
@@ -1,36 +1,15 @@
 import Cell
 import Position as ps
-import heapq as hq
 
 GENERATIONS = 30
 SIMULATION_STEPS = 50
 
 
-def getLargest(cells):
+def get_largest(cells):
     return cells[len(cells)-1], cells[len(cells)-2]
 
-if __name__ == "__main__":
 
-    # Print and recombinate tests
-
-    cellID = 0
-
-    cell_a = Cell.Cell(cellID, ps.Position(0, cellID))
-    cellID += 1
-    cell_b = Cell.Cell(cellID, ps.Position(0, cellID))
-    cellID += 1
-    cell_c = Cell.Cell(cellID, ps.Position(0, cellID))
-    cellID += 1
-    cell_d = Cell.Cell(cellID, ps.Position(0, cellID))
-    cellID += 1
-
-    cell_e = Cell.Cell(cellID, ps.Position(0, cellID), cell_a, cell_b)
-    cellID += 1
-    cell_f = Cell.Cell(cellID, ps.Position(0, cellID), cell_c, cell_d)
-    cellID += 1
-    # Interaction Tests
-
-    allCells = [cell_a, cell_b, cell_c, cell_d, cell_e, cell_f]
+def interaction_tests( cell_a, cell_b, cell_c, cell_d, cell_e, cell_f ):
 
     neighbours = [cell_b, cell_c, cell_d]
     cell_a.interact(neighbours)
@@ -71,6 +50,29 @@ if __name__ == "__main__":
     if cell_d.hasInteracted(cell_b): print "err: cell_d, cell_b"
     if cell_d.hasInteracted(cell_c): print "err: cell_d, cell_c"
 
+if __name__ == "__main__":
+
+    # Print and recombinate tests
+    cellID = 0
+
+    cell_a = Cell.Cell(cellID, ps.Position(0, cellID))
+    cellID += 1
+    cell_b = Cell.Cell(cellID, ps.Position(0, cellID))
+    cellID += 1
+    cell_c = Cell.Cell(cellID, ps.Position(0, cellID))
+    cellID += 1
+    cell_d = Cell.Cell(cellID, ps.Position(0, cellID))
+    cellID += 1
+
+    cell_e = Cell.Cell(cellID, ps.Position(0, cellID), cell_a, cell_b)
+    cellID += 1
+    cell_f = Cell.Cell(cellID, ps.Position(0, cellID), cell_c, cell_d)
+    cellID += 1
+
+    # Interaction Tests
+    allCells = [cell_a, cell_b, cell_c, cell_d, cell_e, cell_f]
+    interaction_tests(cell_a, cell_b, cell_c, cell_d, cell_e, cell_f)
+
     avg_def = 0
     initial_move_percent = 0
     totalScore = 0
@@ -92,16 +94,14 @@ if __name__ == "__main__":
                 cell.clearInteractions()
             for cell in allCells:
                     cell.interact(allCells)
-            #for cell in allCells:
-                #if cell.isDead():
-                #    allCells.remove(cell)
+            # for cell in allCells:
+            #     if cell.isDead():
+            #         allCells.remove(cell)
         allCells.sort(key=lambda c: c._score)
         allCells.remove(allCells[0])
-        best_cell_a, best_cell_b = getLargest(allCells)
+        best_cell_a, best_cell_b = get_largest(allCells)
         allCells.append(Cell.Cell(cellID, cellID, best_cell_a, best_cell_b))
         cellID += 1
-
-    print "\nALL CELLS:\n"
 
     avg_def = 0
     initial_move_percent = 0
@@ -116,17 +116,15 @@ if __name__ == "__main__":
         print "Initial move %defect: " + str(float(initial_move_percent)/float(len(allCells)))
         print "Average score: " + str(float(totalScore)/float(len(allCells)))
 
+    cell_1, cell_2 = get_largest(allCells)
+    print "\nBest Cells: \n"
+    print "a:" + str(cell_1)
+    print "b: " + str(cell_2)
+
     allCells.sort(key=lambda c: c._score)
     for cell in allCells:
         print str(cell)
 
-    if 2 <= len(allCells):
-        cell_1, cell_2 = getLargest(allCells)
-        print "\nBest Cells: \n"
-        print "a:" + str(cell_1)
-        print "b: " + str(cell_2)
-    else:
-        print str(allCells)
 
 
 
