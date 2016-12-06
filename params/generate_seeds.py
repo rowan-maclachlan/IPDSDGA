0ab88e301e49e5465fc03f902194d4b3388e4c09
Jarrod Pas <jarrodpas@gmail.com>
2016-12-06 14:36:16 -0600

    Add more model sizes

--- params/generate_seeds.py
+++ params/generate_seeds.py
@@ -1,15 +1,20 @@
+#!/usr/bin/env python3
+
 import json
 import copy
 
-base = {
-    'surface': {
-        'height': 10,
-        'width': 10
-    }
-}
+if __name__ == '__main__':
+    import sys
 
-for seed in range(0, 8):
-    params = copy.deepcopy(base)
-    params['random_seed'] = seed
-    with open('small_{}.json'.format(seed), 'w+') as f:
-        json.dump(params, f, indent=4, sort_keys=True)
+    base = {
+        'surface': {
+            'height': int(sys.argv[2]),
+            'width': int(sys.argv[2])
+        }
+    }
+
+    for seed in range(0, 8):
+        params = copy.deepcopy(base)
+        params['random_seed'] = seed
+        with open('{}_{}.json'.format(sys.argv[1], seed), 'w+') as f:
+            json.dump(params, f, indent=4, sort_keys=True)
