e9a9365da505982bfe3dd5cb61b2f813bd801796
Jarrod Pas <jarrodpas@gmail.com>
2016-12-06 18:11:25 -0600

    Prevents running more than nproc simulations

--- run-batch.sh
+++ run-batch.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+max=$(nproc 2> /dev/null)
+max=${max:-8}
+
 for p in $@; do
+    while [[ "$(screen -ls | grep cmpt394 | wc -l)" -gt "$max" ]]; do
+        sleep 1
+    done
+    echo "Starting $(basename -s .json $p)"
     screen -dmS "cmpt394_$(basename -s .json $p)" bash -c "./run.sh $p"
 done
 
