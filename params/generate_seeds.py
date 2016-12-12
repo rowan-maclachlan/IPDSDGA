acbcdb271f0e487f5fbe53c93cc7608d4bebb1aa
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2016-12-12 17:12:35 -0600

    moved fiels to old

--- params/generate_seeds.py
+++ params/generate_seeds.py
@@ -6,15 +6,12 @@ import copy
 if __name__ == '__main__':
     import sys
 
-    base = {
-        'surface': {
-            'height': int(sys.argv[2]),
-            'width': int(sys.argv[2])
-        }
-    }
+    params = {}
+    name = "vary_bleed"
 
-    for seed in range(0, 8):
-        params = copy.deepcopy(base)
-        params['random_seed'] = seed
-        with open('{}_{}.json'.format(sys.argv[1], seed), 'w+') as f:
-            json.dump(params, f, indent=4, sort_keys=True)
+    for seed in range(0, 10):
+        for x in range(0, 10):
+            params['random_seed'] = seed
+            params['loss_per_tick'] = 2.0 + float(x) / 10
+            with open('{}_{}_{}.json'.format(name, seed, x), 'w+') as f:
+                json.dump(params, f, indent=4, sort_keys=True)
