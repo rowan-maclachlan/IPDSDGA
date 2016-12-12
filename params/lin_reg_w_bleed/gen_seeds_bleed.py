b22f843cb6e9a159eee1e2f69cc796ae7fa20e89
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2016-12-12 17:26:42 -0600

    moved data around

--- params/lin_reg_w_bleed/gen_seeds_bleed.py
+++ params/lin_reg_w_bleed/gen_seeds_bleed.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     name = "vary_bleed"
 
     for seed in range(0, 10):
-        for x in range(0, 10):
+        for x in range(0, 30):
             params['random_seed'] = seed
-            params['loss_per_tick'] = 2.0 + float(x) / 10
-            with open('{}_{}_{}.json'.format(name, seed, x), 'w+') as f:
+            params['loss_per_tick'] = 1.0 + float(x)/10
+            with open('{}_s{}_b{}.json'.format(name, seed, x), 'w+') as f:
                 json.dump(params, f, indent=4, sort_keys=True)
