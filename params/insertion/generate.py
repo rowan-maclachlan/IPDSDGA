6c0d7cf115fc0ef2718ddd68e82b37d7ba697daf
Jarrod Pas <jarrodpas@gmail.com>
2016-12-12 20:22:19 -0600

    Adds insertion variation

---
+++ params/insertion/generate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import json
+
+def write(params, name):
+    with open(name, 'w+') as f:
+        json.dump(params, f, indent=4, sort_keys=True)
+
+if __name__ == '__main__':
+    params = {}
+    name = "insert_{f}_{seed}.json"
+    for seed in range(10):
+        params['random_seed'] = seed
+        for f in range(0, 105, 5):
+            params['mutation_cance_insert'] = f / 100.0
+            write(params, name.format(f=f,seed=seed))
