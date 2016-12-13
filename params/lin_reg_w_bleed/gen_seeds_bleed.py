1b08d22031808d445bc2322cf5da2786c2a182a6
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2016-12-13 12:57:36 -0600

    Comments

--- params/lin_reg_w_bleed/gen_seeds_bleed.py
+++ params/lin_reg_w_bleed/gen_seeds_bleed.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-
+"""
+Generate 10 seeds for parameter files for
+values of loss-per-tick from 1.0 to 3.9 in 0.1
+step increments.
+"""
 import json
 import copy
 
@@ -7,7 +11,7 @@ if __name__ == '__main__':
     import sys
 
     params = {}
-    name = "vary_bleed"
+    name = "vb"
 
     for seed in range(0, 10):
         for x in range(0, 30):
